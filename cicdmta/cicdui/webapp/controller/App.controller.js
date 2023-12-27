8745d85bed83a0927a7704ee7f75d3e492746054
imran khan mohammad <imran.endeavour@gmail.com>
2023-12-27 13:41:51 +0000

    init

---
+++ cicdmta/cicdui/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("ci.cd.cicdui.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ cicdmta/cicdui/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ci/cd/cicdui/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
